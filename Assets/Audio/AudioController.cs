8b6785f049c362c0b0335140e92ed9e328853e7f
zziedss <rhv.martins@gmail.com>
2024-01-28 12:42:25 +0200

    fhgfh

---
+++ Assets/Audio/AudioController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public enum AudioTypes
+{
+    None = 0,
+    Background,
+    Particle,
+    // Gun
+    // Blood
+
+}
+
+
+
+public class AudioController : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
